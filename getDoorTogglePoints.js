1165f8cc2dd15186a2f55e7733825da0ec39abf3
floodpeak <hongfengzhou@yahoo.com>
2016-10-22 23:45:58 +0800

    rf, make the candidate change for 6 and 8

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -1,14 +1,16 @@
 export default (door)=>{
   const result = [1]
-  const candidate = 2
+  let candidate = 2
   if(door % candidate === 0 && door !== candidate){
     result.push(candidate)
   }
+  candidate ++
   if(door === 6){
-    result.push(3)
+    result.push(candidate)
   }
+  candidate ++
   if(door === 8){
-    result.push(4)
+    result.push(candidate)
   }
   if(door > 1){
     result.push(door)
